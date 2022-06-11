066a79dbfda5d4393aac6c391d9120a54163e69e
atom <erolsancaktutan@gmail.com>
2022-06-12 00:45:05 +0300

    restore

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -2,13 +2,13 @@
 #include <string>
 
 extern "C"
-JNIEXPORT jstring JNICALL
+JNIEXPORT jstring  extern "C" jstring
 Java_com_es_news_utility_Constants_getBaseURL(JNIEnv *env, jobject thiz) {
     std::string baseUrl = "https://newsapi.org/";
     return env->NewStringUTF(baseUrl.c_str());
 }
 extern "C"
-JNIEXPORT jstring JNICALL
+JNIEXPORT jstring  extern "C" jstring
 Java_com_es_news_utility_Constants_getApiKey(JNIEnv *env, jobject thiz) {
     std::string apiKey = "9390c529f6aa4cb9a7be3d19832cd2d5";
     return env->NewStringUTF(apiKey.c_str());
